f36c34d40049138650b5b666d9a3e61055fdbdfc
Fantomaser <49395532+Fantomaser@users.noreply.github.com>
2020-01-21 16:46:48 +0300

    Try atom

--- streams/mainStream.go
+++ streams/mainStream.go
@@ -2,11 +2,19 @@ package streams
 
 import (
 	"fmt"
+	"image"
+	//"os"
 
 )
+
+var pict image.Image = image.Image{}
 
 //LogicStreamGo is main stream encapsulating other engine stream
 func LogicStreamGo(token chan<- int64) {
 	fmt.Scanln()
 	token <- 0
+}
+
+func SaveJpeg() {
+
 }
